ce9e7ca9891d4b574b7390bbca11716170bd8cb3
PixelMCN <pixelmcn@mazecraftmc.fun>
2026-03-05 20:43:33 +0530

    updated

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -46,7 +46,7 @@ export default function ContactSection() {
                 >
                     {/* WhatsApp */}
                     <a
-                        href="https://wa.me/919876543210"
+                        href="https://wa.me/917595887171"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
